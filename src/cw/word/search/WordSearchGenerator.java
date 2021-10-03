d098dd674a240cdc84d5179bd59382e5910cd760
Chris Wang <chrisw.msf@gmail.com>
2021-10-02 22:43:10 -0500

    Implemented code for adding words to word search grid

--- src/cw/word/search/WordSearchGenerator.java
+++ src/cw/word/search/WordSearchGenerator.java
@@ -4,8 +4,15 @@ import java.util.*;
 
 public class WordSearchGenerator {
 	
+	// min and max length of square word search grid
 	public static final int minSize = 5;
 	public static final int maxSize = 20;
+	
+	// minimum acceptable word length
+	public static final int minWordLength = 2;
+	
+	// number of times to try adding a word to the grid before giving up
+	public static final int wordRetry = 10;
 
 	public static void main(String[] args) {
 		
@@ -26,19 +33,88 @@ public class WordSearchGenerator {
 	    }
 	    
 	    // read in grid length
-		System.out.println("Enter the side length of the word search grid (between 5 and 20):");
+		System.out.println("Enter the side length of the word search grid (between " + minSize + " and " + maxSize + "):");
 		String sizeString = in.nextLine();
 		
-		// check if size is valid, and if not, then re-prompt and retry
-		while (!sizeIsValid(sizeString)) {
-			System.out.println("Invalid input! Enter the side length of the word search grid (between 5 and 20):");
+		// check if size is valid int and within bounds, and if not, then re-prompt and retry
+		while ((!stringIsNatural(sizeString)) || Integer.parseInt(sizeString) < minSize || Integer.parseInt(sizeString) > maxSize) {
+			System.out.println("Invalid input! Enter the side length of the word search grid (between " + minSize + " and " + maxSize + "):");
 			sizeString = in.nextLine();
+		}
+		int size = Integer.parseInt(sizeString);
+		
+		System.out.println("Enter the number of words to use in the word search:");
+		String wordNumber = in.nextLine();
+		
+		// check if word number is valid int and less than total number of words
+		// if not, then re-prompt and retry
+		boolean wordNumberIsNat = stringIsNatural(wordNumber);
+		while ((!wordNumberIsNat) || Integer.parseInt(wordNumber) >= words.size()) {
+			if (!wordNumberIsNat) {
+				System.out.println("Invalid input! Enter the number of words to use in the word search:");
+			}
+			else {
+				System.out.println("Invalid input! The number you entered is greater than the total number of words in the list.");
+				System.out.println("Enter the number of words to use in the word search:");
+			}
+			wordNumber = in.nextLine();
+			wordNumberIsNat = stringIsNatural(wordNumber);
+		}
+		int wordNo = Integer.parseInt(wordNumber);
+		
+		// print message after all user inputs are taken in
+		System.out.println("Generating " + size + " x " + size + " word search using " + wordNo + " out of " + words.size() + " words...");
+		in.close();
+	    
+		// initialize objects for generating the puzzle and lists of words used and answers
+		char[][] grid = new char[size][size];
+		ArrayList<String> wordsUsed = new ArrayList<>();
+		HashSet<String> answers = new HashSet<>();
+		Random r = new Random();
+		
+		// select each word randomly and add it to the grid, one at a time
+		for (int i = 0; i < wordNo; i++) {
+			
+			// if all words have run out (due to too many being invalid), 
+			if (words.isEmpty()) {
+				
+				// if zero words were added, terminate the program
+				if (wordsUsed.isEmpty()) {
+					System.out.println("Error: A word search could not be generated! All of your words are too short or too long. Please try again with a new list.");
+					System.out.println("Terminating program.");
+					return;
+				}
+				
+				
+			}
+			
+			// get a random word and remove it from the list
+			int index = r.nextInt(words.size());
+			String w = words.get(index);
+			words.remove(index);
+			
+			// if the word is too short or too long, it cannot be used
+			// continue and retry with the next word
+			if (w.length() > size || w.length() < minWordLength) {
+				i--;
+				continue;
+			}
+			
+			int count = 0;
+			while ((!addWordToGrid(w, grid, wordsUsed, answers)) && count < wordRetry) {
+				count++;
+			}
+			
+			if (count == wordRetry) {
+				System.out.println("Adding word " + w + " failed...");
+				i--;
+				continue;
+			}
+			
+			
 		}
 		
 		
-		
-		in.close();
-	    
 	}
 	
 	public static boolean wordList(String filename, ArrayList<String> words) {
@@ -46,8 +122,15 @@ public class WordSearchGenerator {
 	    try {
 	    	BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
 		    String line;
+		    boolean empty = true;
 		    while ((line = br.readLine()) != null) {
 		    	words.add(line);
+		    	empty = false;
+		    }
+		    
+		    if (empty) {
+		    	System.out.println("Error: File is empty!");
+				return true;
 		    }
 		    
 		} catch (FileNotFoundException e) {
@@ -62,7 +145,8 @@ public class WordSearchGenerator {
 	    return false;
 	}
 	
-	public static boolean sizeIsValid(String s) {
+	public static boolean stringIsNatural(String s) {
+		// checks if a string is a natural number (positive integer)
 		
 		// if string is too long, it is not valid
 		if (s.length() > 9) {
@@ -76,9 +160,128 @@ public class WordSearchGenerator {
 			}
 		}
 		
-		// check if size is within bounds
-		return (Integer.parseInt(s) >= minSize && Integer.parseInt(s) <= maxSize);
+		// only natural number if non-zero
+		return Integer.parseInt(s) != 0;
 		
+	}
+	
+	public static boolean addWordToGrid(String w, char[][] grid, ArrayList<String> wordsUsed, HashSet<String> answers) {
+		// adds word to grid, returns true and adds it to wordsUsed and answers if successful
+		// returns false if unsuccessful due to conflict with other existing word
+		
+		int size = grid.length;
+		int startX, startY;
+		Random r = new Random();
+		
+		// select a valid starting point in the grid at random
+		// if w.length() <= size/2 + 1, then any starting point is valid
+		if (w.length() < size/2 + 1) {
+			startX = r.nextInt(size);
+			startY = r.nextInt(size);
+		}
+		
+		// otherwise, the starting point has to be picked near the edges of the grid
+		else {
+			// randomly choose to pick startX on left or right side
+			if (r.nextInt(2) == 0) {
+				// pick startX on left side
+				startX = r.nextInt(size-w.length()+1);
+			}
+			else {
+				// pick startX on right side
+				startX = size - 1 - r.nextInt(size-w.length()+1);
+			}
+			
+			// do the same for picking startY
+			if (r.nextInt(2) == 0) {
+				// pick startY on top side
+				startY = r.nextInt(size-w.length()+1);
+			}
+			else {
+				// pick startY on bottom side
+				startY = size - 1 - r.nextInt(size-w.length()+1);
+			}
+		}
+		
+		// must determine valid directions for word
+		ArrayList<Integer> directions = new ArrayList<>();
+		boolean left = (startX + 1 >= w.length());
+		boolean right = (size - startX >= w.length());
+		boolean up = (startY + 1 >= w.length());
+		boolean down = (size - startY >= w.length());
+		
+		// correspondence between directions and integer values
+		if (up) {
+			directions.add(1);
+		}
+		if (up && right) {
+			directions.add(2);
+		}
+		if (right) {
+			directions.add(3);
+		}
+		if (right && down) {
+			directions.add(4);
+		}
+		if (down) {
+			directions.add(5);
+		}
+		if (down && left) {
+			directions.add(6);
+		}
+		if (left) {
+			directions.add(7);
+		}
+		if (left && up) {
+			directions.add(8);
+		}
+		
+		// this code should not ever run if the math is correct
+		if (directions.size() == 0) {
+			System.out.println("An error occurred with placing a word...");
+			return false;
+		}
+		
+		// pick a random direction out of the valid directions
+		int dir = directions.get(r.nextInt(directions.size()));
+		
+		// attempts to place word in grid
+		int x = startX;
+		int y = startY;
+		for (int i = 0; i < w.length(); i++) {
+			
+			// if the cell is blank (or already contains the correct letter), add the letter
+			// and increment x and y according to the direction
+			if (grid[x][y] == 0 || grid[x][y] == w.charAt(i)) {
+				
+				grid[x][y] = w.charAt(i);
+				
+				if (dir == 6 || dir == 7 || dir == 8) {
+					x--; // left
+				}
+				else if (dir == 2 || dir == 3 || dir == 4) {
+					x++; // right
+				}
+				if (dir == 1 || dir == 2 || dir == 8) {
+					y--; // up
+				}
+				else if (dir == 4 || dir == 5 || dir == 6) {
+					y++; // down
+				}
+			}
+			
+			// otherwise, there is a conflict and we need to stop
+			else {
+				return false;
+			}
+			
+		}
+		
+		// if the word was placed in the grid, add it to the word and answer lists
+		wordsUsed.add(w);
+		answers.add(w);
+		
+		return true;
 	}
 	
 	
