7a00a9b06982ee85d3493443f028c36ead5ec85f
Chris Wang <chrisw.msf@gmail.com>
2021-10-03 16:27:22 -0500

    Updated error message

--- src/wordsearch/Messages.java
+++ src/wordsearch/Messages.java
@@ -14,7 +14,7 @@ public class Messages {
 	public static final String fileEmpty = "Error: File is empty!";
 	public static final String fileNotFound = "Error: File not found!";
 	public static final String fileReadFailed = "Error: File read failed!";
-	public static final String puzzleFailed = "Error: A word search could not be generated! All of your words are too short or too long. Please try again with a new list.";
+	public static final String puzzleFailed = "Error: A word search could not be generated! Please try again with a new list or a different grid size.";
 	public static final String termination = "Terminating program.";
 	public static final String unexpectedError = "An unexpected error occurred when placing a word...";
 	
